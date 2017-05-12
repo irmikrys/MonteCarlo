7a794187067fe995dad0e62781465a1c270ec562
Irmina Krysiak <irmina.krysiak@gmail.com>
2017-05-12 16:53:47 +0200

    Algo random arrs, check concept

--- src/app/Algo.java
+++ src/app/Algo.java
@@ -14,15 +14,34 @@ public class Algo {
     public static String maxMinTarget;
     public static ArrayList<LimitField> limits;
 
-    private static ArrayList<Double> randomCoordinates = new ArrayList<>();
+    private static ArrayList<ArrayList<Double>> randomArrs = new ArrayList<>();
+
     private static final double MIN = Double.MIN_VALUE;
     private static final double MAX = Double.MAX_VALUE;
+    private static final double POINTS_NUM = 1000;
 
+    //n = limits.size
     private static ArrayList<Double> genRandomXs(int n) {
         ArrayList<Double> randomArray = new ArrayList<>(n);
         for(int i = 0; i < n; i++){
             randomArray.set(i, ThreadLocalRandom.current().nextDouble(MIN, MAX));
         }
         return randomArray;
+    }
+
+    //randomArrsNum = POINTS_NUM, n = limits.size
+    private static void fillWithRandomArrs(int pointsNum, int n) {
+        for(int i = 0; i < pointsNum; i++){
+            randomArrs.add(genRandomXs(n));
+        }
+    }
+
+    //dla kazdego punktu tak bedzie sprawdzane
+    private static boolean checkLimits(int limitsNum, int pointsNum) {
+        for(int i = 0; i < limitsNum; i++){
+            //check if condition is ok
+            //if not ok any, false
+        }
+        return true;
     }
 }
