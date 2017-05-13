cf1186bf006ae3afb1cfd526ab7670a6a7eb3933
Irmina Krysiak <irmina.krysiak@gmail.com>
2017-05-13 23:53:57 +0200

    getNeighbors of point method added to Point

--- src/app/algo/Point.java
+++ src/app/algo/Point.java
@@ -1,6 +1,7 @@
 package app.algo;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created on 13.05.2017.
@@ -14,5 +15,20 @@ public class Point {
 
     public Point(ArrayList<Double> coordinates) {
         this. coordinates = coordinates;
+    }
+
+    //zwraca neighborsNum sasiadow w promieniu radius od punktu
+    public ArrayList<Point> getNeighbors(int neighborsNum, double radius) {
+        ArrayList<Point> neighbors = new ArrayList<>(neighborsNum);
+        for(int i = 0; i < neighborsNum; i++) {
+            Point point = new Point(coordinates.size());
+            for(int j = 0; j < coordinates.size(); j++) {
+                double minTmp = coordinates.get(j) - radius;
+                double maxTmp = coordinates.get(j) + radius;
+                point.coordinates.set(j, ThreadLocalRandom.current().nextDouble(minTmp, maxTmp));
+            }
+            neighbors.add(point);
+        }
+        return neighbors;
     }
 }
